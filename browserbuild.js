0cda08c04fc01f43462c091c20688b9c569be075
maple3142 <kirby741852963@gmail.com>
2018-02-21 22:03:08 +0800

    update build

---
+++ browserbuild.js
@@ -0,0 +1,17 @@
+// because browser build don't need "node.js"
+
+const fs = require('fs')
+const Bili = require('bili')
+
+const indexjs = __dirname + '/src/index.js'
+const index2js = __dirname + '/src/index2.js'
+
+const ct = fs.readFileSync(indexjs, 'utf-8')
+fs.writeFileSync(index2js, ct.replace('^.*node.*$'), 'utf-8')
+
+Bili.write({
+	input: index2js,
+	format: ['umd', 'umd-min']
+}).then(() => {
+	fs.unlinkSync(index2js)
+})
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as common from './common'
 
-export default {
+module.exports = {
 	common
 }
